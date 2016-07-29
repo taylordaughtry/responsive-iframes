183881423d941cae38de3d32a2818169c4415163
Taylor Daughtry <hello@taylordaughtry.com>
2016-07-28 19:20:11 -0500

    Optimization

--- fitvids-native.js
+++ fitvids-native.js
@@ -5,33 +5,35 @@ var fitVids = {};
 
 	var _buildStyle = function() {
 		var div = document.createElement('div'),
-			css = '.fluid-wrapper{width:100%;position:relative;padding:0;}.fluid-wrapper iframe,.fluid-wrapper object,.fluid-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}',
-			head = document.getElementsByTagName('head')[0];
+			css = '.fluid-wrapper{width:100%;position:relative;padding:0;}.fluid-wrapper iframe,.fluid-wrapper object,.fluid-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
 
 		div.innerHTML = '<style>' + css + '</style>';
 
-		head.appendChild(div);
+		document.getElementsByTagName('head')[0].appendChild(div);
 
 		return div;
 	};
 
-	FV.respondify = function(selector) {
-		var container = document.getElementsByClassName(selector.substr(1))[0],
-			videos = container.getElementsByTagName('iframe'),
-			styleDiv = _buildStyle(),
-			wrapper = document.createElement('div');
+	var _render = function(element) {
+		var wrapper = document.createElement('div');
 
 		wrapper.className = 'fluid-wrapper';
 
-		for (var i = 0; i < videos.length; i++) {
-			var el = videos[i],
-				ratio = videos[i].getAttribute('height') / videos[i].getAttribute('width');
+		element.parentNode.insertBefore(wrapper, element.nextSibling);
 
-			el.parentNode.insertBefore(wrapper, el.nextSibling);
+		wrapper.appendChild(element);
 
-			wrapper.appendChild(el);
+		element.parentNode.style.paddingTop = (element.getAttribute('height') / element.getAttribute('width')) * 100 + '%';
+	};
 
-			el.parentNode.style.paddingTop = ratio * 100 + '%';
+	FV.init = function(selector) {
+		var query = document.querySelectorAll(selector),
+			elements = query[0] ? query : document.getElementsByTagName('iframe');
+
+		_buildStyle();
+
+		for (var i = 0; i < elements.length; i++) {
+			_render(elements[i]);
 		}
 	};
 
